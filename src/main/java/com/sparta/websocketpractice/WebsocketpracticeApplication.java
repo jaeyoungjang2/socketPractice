7d9c7e9909ed8bf5b9095d865c68af7a9d6ee02d
jaeyoungjang2 <lion85488@gmail.com>
2022-04-27 20:21:45 +0900

    readme 추가

---
+++ src/main/java/com/sparta/websocketpractice/WebsocketpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.websocketpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketpracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/websocketpractice/WebsocketpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.websocketpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
